fa36f371782d0bbf9f3485d970a5b1ab2afd441f
ViniciusProgrammer <viniprogrammer@hotmail.com>
2025-10-05 12:37:06 -0300

    refactor: Adicionando comentários Importância dos comentários no java, evitar usar comentários multiplas linhas, preferir javadoc e evitar o máximo comentários de única linha

--- src/academy/davdojo/maratonajava/introducao/OlaDevDojo.java
+++ src/academy/davdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,6 +1,14 @@
 package academy.davdojo.maratonajava.introducao;
 
+/**
+ * Isso é um comentário utilizando <strong>javadoc</strong> podemos usar até html aqui
+ */
 public class OlaDevDojo {
+    // Comentários podem ser adicionados aqui como linha feito esse utilizando barra barra //
+
+    /*
+        Comentários de múltiplas linhas são feitos dessa forma
+     */
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HAAA");
     }
