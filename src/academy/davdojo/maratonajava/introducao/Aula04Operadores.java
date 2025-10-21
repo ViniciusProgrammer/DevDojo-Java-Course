ad37aa768ea11f1cfd6f92d247553e357741360f
ViniciusProgrammer <viniprogrammer@hotmail.com>
2025-10-20 21:31:25 -0300

    Mais operadores

--- src/academy/davdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/davdojo/maratonajava/introducao/Aula04Operadores.java
@@ -58,5 +58,91 @@ public class Aula04Operadores {
         int diferenca = maiorValor - menorValor;
 
         System.out.println(diferenca);
+
+        /**
+         * Operador do resto da divisão %
+         */
+
+        // par
+        int resto = 20 % 2;
+        System.out.println(resto);
+
+        // impar
+        int resto2 = 21 % 2;
+        System.out.println(resto2);
+
+        /**
+         * Operadores relacionais <, >, <=, >=, == e != sempre retornaram valores booleanos
+         * < menor
+         * > maior
+         * <= menor ou igual
+         * >= maior ou igual
+         * == igual
+         * != diferente
+         */
+
+        boolean dezMenorQueVinte = 10 < 20; // true
+        boolean dezMaiorQueVinte = 10 > 20; // false
+        boolean dezIgualVinte = 10 == 20; // false
+        boolean dezDiferenteVinte = 10 != 20; // true
+        boolean dezIgualDez = 10 == 10; // true
+
+        System.out.println(dezMenorQueVinte);
+        System.out.println(dezMaiorQueVinte);
+        System.out.println(dezIgualVinte);
+        System.out.println(dezDiferenteVinte);
+        System.out.println(dezIgualDez);
+
+        /**
+         * Operadores lógicos
+         * && (AND) || (OU) ! (NÃO)
+         */
+
+        // AND
+
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
+
+        // OR
+
+        double valorTotalContaCorrente = 200.0;
+        double valorTotalContaPoupanca = 10000.0;
+        float valorPlastation = 5000F;
+        boolean isPlaystation5Compravel = valorTotalContaCorrente > valorPlastation || valorTotalContaPoupanca > valorPlastation;
+
+        System.out.println("isPlastation5Compravel? " + isPlaystation5Compravel);
+
+        /**
+         * Operadores de atribuição
+         * =, +=, -=, *=, /=, %=
+         */
+
+        double bonus = 1800;
+        bonus = bonus + 200;
+
+        System.out.println("Bonus com atribuição: " + bonus);
+
+        bonus -= 502;
+        System.out.println("Bonus com atribuição subtrativa: " + bonus);
+
+        bonus *= 3;
+        System.out.println("Bonus com atribuição multiplicativa: " + bonus);
+
+        bonus /= 2;
+        System.out.println("Bonus com atriubuição divisiva: " + bonus);
+
+        bonus %= 2;
+        System.out.println("Bonus com atribuição modulo: " + bonus);
+
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        System.out.println("Contador: " + contador);
     }
 }
