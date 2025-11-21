6f6e922e8be7647ca425f4195896c4be9c8e099d
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2025-11-21 10:39:28 -0300

    Associação, unidirecional

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest02 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Time selecao = new Time("Seleção brasileira");
+
+        jogador1.setTime(selecao);
+
+        jogador1.imprime();
+    }
+}
