446e4ecefdf582cc2449e4506a47136f73e3b3c1
ViniciusProgrammer <viniciusimdufrn@gmail.com>
2025-11-25 13:58:28 -0300

    Conceitos de heranças no java

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("55555666-000");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Toyihisa Shimazu");
+        pessoa.setCpf("7885421215-56");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Oda Nobunaga");
+        funcionario.setCpf("894453121-51");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000.00);
+
+        System.out.println("=============================");
+        funcionario.imprime();
+    }
+}
